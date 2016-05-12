c16d7bb8b48d0553db0b750376d810cb22b01ab8
bdrumheller <drumhb@rpi.edu>
2016-05-11 20:45:58 -0400

    refine mesh and some of the quad nets

---
+++ loop.py
@@ -0,0 +1,337 @@
+import bpy, bmesh, sympy
+import mathutils
+from mathutils import Vector
+
+#Provided from the Blender Wiki
+
+def createMeshFromData(name, origin, verts, faces):
+    # Create mesh and object
+    me = bpy.data.meshes.new(name+'Mesh')
+    ob = bpy.data.objects.new(name, me)
+    ob.location = origin
+    ob.show_name = True
+ 
+    # Link object to scene and make active
+    scn = bpy.context.scene
+    scn.objects.link(ob)
+    scn.objects.active = ob
+    ob.select = True
+ 
+    # Create mesh from given verts, faces.
+    me.from_pydata(verts, [], faces)
+    # Update mesh with new data
+    me.update()    
+    return ob
+ 
+def createMeshFromOperator(name, origin, verts, faces):
+    bpy.ops.object.add(
+        type='MESH', 
+        enter_editmode=False,
+        location=origin)
+    ob = bpy.context.object
+    ob.name = name
+    ob.show_name = True
+    me = ob.data
+    me.name = name+'Mesh'
+ 
+    # Create mesh from given verts, faces.
+    me.from_pydata(verts, [], faces)
+    # Update mesh with new data
+    me.update()    
+    # Set object mode
+    bpy.ops.object.mode_set(mode='OBJECT')
+    return ob
+ 
+def createMeshFromPrimitive(name, origin):
+    bpy.ops.mesh.primitive_cone_add(
+        vertices=4, 
+        radius=1, 
+        depth=1, 
+        cap_end=True, 
+        view_align=False, 
+        enter_editmode=False, 
+        location=origin, 
+        rotation=(0, 0, 0))
+ 
+    ob = bpy.context.object
+    ob.name = name
+    ob.show_name = True
+    me = ob.data
+    me.name = name+'Mesh'
+    return ob
+ 
+def createArmatureFromData(name, origin):
+    # Create armature and object
+    amt = bpy.data.armatures.new(name+'Amt')
+    ob = bpy.data.objects.new(name, amt)
+    ob.location = origin
+    ob.show_name = True
+ 
+    # Link object to scene and make active
+    scn = bpy.context.scene
+    scn.objects.link(ob)
+    scn.objects.active = ob
+    ob.select = True
+ 
+    # Create single bone
+    bpy.ops.object.mode_set(mode='EDIT')
+    bone = amt.edit_bones.new('Bone')
+    bone.head = (0,0,0)
+    bone.tail = (0,0,1)
+    bpy.ops.object.mode_set(mode='OBJECT')
+    return ob
+ 
+def createArmatureFromOperator(name, origin):
+    bpy.ops.object.add(
+        type='ARMATURE', 
+        enter_editmode=True,
+        location=origin)
+    ob = bpy.context.object
+    ob.name = name
+    ob.show_name = True
+    amt = ob.data
+    amt.name = name+'Amt'
+ 
+    # Create single bone
+    bone = amt.edit_bones.new('Bone')
+    bone.head = (0,0,0)
+    bone.tail = (0,0,1)
+    bpy.ops.object.mode_set(mode='OBJECT')
+    return ob
+ 
+def createArmatureFromPrimitive(name, origin):
+    bpy.ops.object.armature_add()
+    bpy.ops.transform.translate(value=origin)
+    ob = bpy.context.object
+    ob.name = name
+    ob.show_name = True
+    amt = ob.data
+    amt.name = name+'Amt'
+    return ob
+
+#End of Wiki Code
+
+def centroid(f_verts):
+    n_verts = len(f_verts)    
+    # find the average value on a face
+    sum = sympy.Matrix(1,3,[0,0,0])
+    for i in range(0,n_verts):
+        sum += f_verts[i]
+    return sum/n_verts
+
+def add_faces_for_edges(edges_to_faces,all_faces, vertex_to_all):
+    # add the faces for each edge
+    edges = list(edges_to_faces.keys())
+    for i in range(0,len(edges_to_faces)):
+        new_face = []
+        # get the faces and find the p primes
+        face1 = True
+        for j in range(0,len(edges_to_faces[edges[i]])):
+            # face per edge
+            f = edges_to_faces[edges[i]][j]
+            #find the new points corresponding to the old point and face
+            edgs = edges[i]
+            
+            if not face1:
+                edgs = reversed(edgs)
+            for k in edgs:
+                new_face.append(vertex_to_all[face_to_new_pt[(f,k)]]) 
+
+            face1 = False          
+        all_faces.append(new_face)   
+        
+def add_faces_for_faces(faces,vertices, all_faces, all_verts):
+    #for each face in the polygon
+    for j in range (0,len(faces)):
+        face = faces[j]
+        f_verts = [sympy.Matrix(1,3,vertices[x]) for x in face]
+        n_verts = len(f_verts)
+        avg = centroid(f_verts)
+        to_add = []
+        
+        # find the corresponding new pt
+        for i in range (0,n_verts):
+            new_pt = tuple(avg/4.0 + f_verts[(i-1)%n_verts]/8.0 
+                    + f_verts[i%n_verts]/2.0 + f_verts[(i+1)%n_verts]/8.0)         
+            i_vert = face[i]   
+            if new_pt not in vertex_to_all.keys():
+                all_vert_len = len(all_verts)
+                to_add.append(len(all_verts))
+                all_verts.append(new_pt) 
+                vertex_to_all[new_pt] = all_vert_len 
+            else:
+                to_add.append(vertex_to_all[new_pt])
+            face_to_new_pt[(j,i_vert)] = new_pt           
+        all_faces.append(to_add)
+
+
+def faces_to_centroids(faces,vertices):
+    centroids = []
+    for i in range(0, len(faces)):
+        face = faces[i]
+        f_verts = [sympy.Matrix(1,3,vertices[x]) for x in face]
+        centroids.append(centroid(f_verts))
+    return centroids
+    
+
+#get the active mesh
+obj = bpy.context.active_object
+
+# Get editmode changes
+obj.update_from_editmode()
+vertices = [tuple(x.co.xyz) for x in obj.data.vertices]
+
+# edges have a start vertex and an end vertex
+
+# get the face of the mesh
+
+mesh = obj.data
+polys = mesh.polygons
+
+faces = [tuple(x.vertices) for x in polys] # vertices of faces
+face_edges = [tuple(x.edge_keys) for x in polys] 
+
+edges_to_faces = {}
+
+for i in range(0, len(face_edges)):
+    fes = face_edges[i]
+    for fe in fes:
+        if fe not in edges_to_faces.keys():
+            edges_to_faces[fe] = []
+        edges_to_faces[fe].append(i)
+
+all_verts = [] # new vertices for the generated surface
+all_faces = [] # new face indicies for the generated surface
+vertex_to_all = {} # mapping to avoid O(n) lookup
+face_to_new_pt = {} # (f_index, pt) -> new pt
+  
+add_faces_for_faces(faces,vertices,all_faces,all_verts)
+add_faces_for_edges(edges_to_faces,all_faces,vertex_to_all)
+
+createMeshFromData('RefinedMesh', Vector((0,0,0)), 
+                            tuple(all_verts), tuple(all_faces))
+                                               
+bpy.ops.object.editmode_toggle()
+#flip the inside out panels
+bpy.ops.mesh.normals_make_consistent(inside=False)
+#add the faces corresponding to vertices
+bpy.ops.object.select_pattern(pattern='RefinedMesh')
+bpy.ops.mesh.edge_face_add()
+bpy.ops.object.editmode_toggle()
+
+quadnets = []
+
+#get the active mesh
+obj = bpy.context.active_object
+# Get editmode changes
+obj.update_from_editmode()
+
+mesh = obj.data
+polys = mesh.polygons
+
+#ridiculous formula result
+def rid_map(f_verts,i):
+    tot = sympy.Matrix(1,3,[0,0,0])
+    n = len(f_verts)
+    beta  = (2/3 * (1 + sympy.cos(2*sympy.pi/n))).evalf()
+    for j in range(0,n):
+        tot = (tot + f_verts[j]*(1.0 + beta*(sympy.cos((2.0*sympy.pi*(j-i)/n)) + sympy.tan(sympy.pi/n)*sympy.sin((2.0*sympy.pi*(j-i))/n)))).evalf()
+    return tot / n
+
+#returns face index, and index on the face
+def vertex_to_faces(faces):
+    vert_to_face = {}
+    for i in range(0,len(faces)):
+        for j in range (0,len(faces[i])):
+            vert = faces[i][j]
+            if vert not in vert_to_face.keys():
+                vert_to_face[vert] = []
+            vert_to_face[vert].append((i,j))
+    
+    """
+    bpy.ops.object.editmode_toggle()
+    bpy.ops.mesh.select_all(action='TOGGLE')
+    mesh=bmesh.from_edit_mesh(bpy.context.object.data)
+    mesh.verts.ensure_lookup_table()
+    for item in vert_to_face.keys():
+        if (len(vert_to_face[item])!= 4):
+            print (vert_to_face[item])
+            mesh.verts[item].select=True
+            # trigger viewport update
+            bpy.context.scene.objects.active = bpy.context.scene.objects.active
+    """
+    return vert_to_face
+
+faces = [tuple(x.vertices) for x in polys] # vertices of face
+vertices = [tuple(x.co.xyz) for x in obj.data.vertices]
+centroids = faces_to_centroids(faces,vertices)
+face_vert_quad_map = {}
+for i in range(0,len(faces)):
+    f_verts = [sympy.Matrix(1,3,vertices[x]) for x in faces[i]]
+    for j in range(0,len(faces[i])):
+        face_vert_quad_map[(i,j)] = rid_map(f_verts,j) #TODO
+    
+vert_to_face = vertex_to_faces(faces)
+
+# for each of the vertices
+# find the surrounding faces 
+# the pt corresponding to the vertex on the face and the counterclockwise point
+
+quad_net = vertices
+quad_face = faces
+
+new_points = {}
+
+for i in range(0,len(vert_to_face)):
+    vert_faces = vert_to_face[i]
+    for j in range(0, len(vert_faces)):
+        #print("Vert faces")
+        #print (vert_faces)
+        to_add = []
+        prev_len = len(quad_net)
+        #vertex -> face_index, index_on_face
+        new_vert = vert_faces[j]
+        face_index = new_vert[0]
+        print("face_index: " + str(face_index))
+        index_on_face = new_vert[1]
+        centroid = tuple(centroids[face_index])
+                
+        offset = -1
+        if centroid not in new_points.keys():   
+            quad_net.append(centroid)
+            new_points[centroid] = prev_len
+            prev_len = prev_len + 1
+            
+        to_add.append(new_points[centroid])
+            
+        n_verts = len(faces[face_index])
+        vert_pt = tuple(face_vert_quad_map[(face_index,(index_on_face+offset)%n_verts)])
+        if vert_pt not in new_points.keys():
+            quad_net.append(vert_pt)
+            new_points[vert_pt] = prev_len
+            prev_len = prev_len + 1
+
+        to_add.append(new_points[vert_pt])
+        
+        # get the clockwise point on the face
+        clockwise_pt = tuple(face_vert_quad_map[(face_index, (index_on_face+offset+1)%n_verts)])
+        if clockwise_pt  not in new_points.keys():
+            quad_net.append(clockwise_pt)
+            new_points[clockwise_pt] = prev_len
+            prev_len = prev_len + 1
+            
+        to_add.append(new_points[clockwise_pt])
+        
+        quad_face.append(tuple(to_add))
+
+print (50*'\n')
+print (quad_net)        
+print ('\n' + str(len(quad_net)))
+
+createMeshFromData('QuadTest', Vector((3,3,0)), 
+                            tuple(quad_net), tuple(quad_face))
+                                             
+                                               
+bpy.ops.object.editmode_toggle()
+#flip the inside out panels
+bpy.ops.mesh.normals_make_consistent(inside=False)
